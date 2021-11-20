0b93b7f1cf89272c92db9601609a90fa42672265
Brian Ujiie <>
2021-11-19 22:11:48 -0800

    Clean up code and make order of slides acknowledge X and Y directions

--- code.ts
+++ code.ts
@@ -1,15 +1,89 @@
-import { makeImage, makeFrame, makeVect2 } from "./lib/factory"
-import { RasterizeMessage } from "./lib/model"
-import { getRasterizeMessages } from "./lib/image"
+interface Slide {
+    x: number
+    y: number
+    width: number
+    height: number
+    bytes: Uint8Array
+}
+
+interface Vect2 {
+    _1: number
+    _2: number
+}
+
+function newVect2(_1: number, _2: number): Vect2 {
+    return { _1, _2 }
+}
+
+function sortPriorityY(nodeA: SceneNode, nodeB: SceneNode): number {
+    const [Ax, Ay, Bx, By] = [nodeA.x, nodeA.y, nodeB.x, nodeB.y]
+    return (Ay == By) ? Ax - Bx : Ay - By
+}
+
+function sortPriorityX(nodeA: SceneNode, nodeB: SceneNode): number {
+    const [Ax, Ay, Bx, By] = [nodeA.x, nodeA.y, nodeB.x, nodeB.y]
+    return (Ax == Bx) ? Ay - By : Ax - Bx
+}
+
+async function convertFrameNodeToImage(node: FrameNode): Promise<Uint8Array> {
+    const nodeBytes: Uint8Array = await node.exportAsync()
+
+    figma.showUI(__html__, { visible: false })
+    figma.ui.postMessage(nodeBytes)
+
+    return await new Promise((resolve, _) => {
+        figma.ui.onmessage = b => resolve(b as Uint8Array)
+    })
+}
+
+async function convertFrameNodeToSlide(node: FrameNode): Promise<Slide> {
+    const imageBytes: Uint8Array = await convertFrameNodeToImage(node)
+    return {
+        x: node.x,
+        y: node.y,
+        width: node.width,
+        height: node.height,
+        bytes: imageBytes
+    }
+}
+
+function newFrame(name: string, pos: Vect2, dim: Vect2): FrameNode {
+    const frame = figma.createFrame()
+    frame.name = name
+    frame.clipsContent = true
+    frame.x = pos._1
+    frame.y = pos._2
+    frame.resize(dim._1, dim._2)
+    return frame
+}
+
+function newImage(name: string, pos: Vect2, dim: Vect2, imageBytes: Uint8Array): RectangleNode {
+    const image = figma.createRectangle()
+    image.name = name
+    image.x = pos._1
+    image.y = pos._2
+    image.resize(dim._1, dim._2)
+    image.fills = [{
+        type: 'IMAGE',
+        scaleMode: 'CROP',
+        imageHash: figma.createImage(imageBytes).hash
+    }]
+    return image
+}
 
 (async function () {
     // We will only act on FrameNodes that are direct children to the
     // current page.
     const frameNodes: Array<FrameNode> = figma.currentPage.children
         .filter(node => node.type == 'FRAME')
-        .sort((a, b) => a.x - b.x) as Array<FrameNode>
+        .sort(sortPriorityY) as Array<FrameNode>
 
-    const rasterizeMessages: Array<RasterizeMessage> = await getRasterizeMessages(frameNodes)
+    // If one of the conversions fail, the Promise.all will discontinue
+    // waiting for the remainder.
+    const slides: Array<Slide> = Array()
+    for (const node of frameNodes) {
+        slides.push(await convertFrameNodeToSlide(node))
+    }
 
     // To keep everything separate, we will add the rasterized FrameNode
     // images to a new, more disposable page.
@@ -20,37 +94,34 @@ import { getRasterizeMessages } from "./lib/image"
     // the Reactions.
     figma.currentPage = newPage
 
-    for (let i = 0; i < rasterizeMessages.length; i++) {
-        const { x, y, width, height } = rasterizeMessages[i].node
-        const paint = rasterizeMessages[i].paint
+    for (const i of Array(slides.length).keys()) {
+        // we need a new frame for each slide. 
+        const { x, y, width, height } = slides[i]
+        const frame = newFrame(`frame ${i}`, newVect2(x, y), newVect2(width, height))
 
-        // for every rasterized image, we want to represent it within
-        // a new frame.
-        const frame = makeFrame(makeVect2(x, y), makeVect2(width, height), false, 'frame' + i)
-        newPage.appendChild(frame)
+        // for each frame, we need variations of the next (1), current (0), and
+        // previous (-1) slide in that order.
+        for (const di of [1, 0, -1]) {
+            const index = i + di
+            // the first slide does not have a previous slide that needs to be
+            // included and the last slide does not have a next slide that needs
+            // to be included.
+            if (index < 0 || index >= slides.length) {
+                continue
+            }
 
-        // If we are not on the last rasterized iamge, we need to
-        // take into account NEXT images when building our frame.
-        if (i < rasterizeMessages.length - 1) {
-            const nextIdx = i + 1
-            const { width, height } = rasterizeMessages[nextIdx].node
-            const paint = rasterizeMessages[nextIdx].paint
-            frame.appendChild(makeImage(paint, makeVect2(0, 0), makeVect2(width, height), 'image' + nextIdx))
-        }
+            const { width, height, bytes } = slides[index]
+            const [x, y] = [0, 0]
 
-        frame.appendChild(makeImage(paint, makeVect2(0, 0), makeVect2(width, height), 'image' + i))
-
-        // If we are past the first rasterized image, that means we now
-        // have to take into account PREVIOUS images when we build our
-        // frame.
-        if (i > 0) {
-            const prevIdx = i - 1
-            const { width, height } = rasterizeMessages[prevIdx].node
-            const paint = rasterizeMessages[prevIdx].paint
-
-            const squeezedWidth = width / 12
-            const padding = 20
-            frame.appendChild(makeImage(paint, makeVect2(-(squeezedWidth + padding), 0), makeVect2(squeezedWidth, height), 'image' + prevIdx))
+            let [adjustedX, adjustedWidth] = [x, width]
+            // If we're working with the previous slide, we need to squish it
+            // horizontally to make the transition look like a squeeze effect.
+            if (di < 0) {
+                adjustedWidth = adjustedWidth / 12
+                adjustedX = -(adjustedWidth + 20)
+            }
+            const image = newImage(`rect ${index}`, newVect2(adjustedX, y), newVect2(adjustedWidth, height), bytes)
+            frame.appendChild(image)
         }
     }
 
